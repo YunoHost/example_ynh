5748f1b18269250bcb033116f9ca80d9812dbbe1
siwinter <45730097+siwinter@users.noreply.github.com>
2019-10-24 12:54:29 +0200

    Create myServer.py

---
+++ myServer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+
+PORT_NUMBER = 8080
+
+#This class will handles any incoming request from
+#the browser 
+class myHandler(BaseHTTPRequestHandler):
+
+        #Handler for the GET requests
+        def do_GET(self):
+                self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+                # Send the html message
+                self.wfile.write("Hello World !")
+                print self.path
+                return
+try:
+        #Create a web server and define the handler to manage the
+        #incoming request
+        server = HTTPServer(('', PORT_NUMBER), myHandler)
+        print 'Started httpserver on port ' , PORT_NUMBER
+
+        #Wait forever for incoming htto requests
+        server.serve_forever()
+
